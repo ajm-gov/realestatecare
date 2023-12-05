3a06ff98b491fb066fee05585f5249e642a234f3
ajm-gov <andrewoverheid@gmail.com>
2023-12-05 20:24:04 +0100

    Some changes

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,3 +1,50 @@
 <template>
-    
+    <ion-list>
+        <ion-item>
+            <ion-avatar slot="start">
+                <img src="@/assets/img/fred.jpeg" width="">
+            </ion-avatar>
+            <ion-label>
+                {{ userName() }}
+            </ion-label>
+        </ion-item>
+        <ion-item>
+                <ion-select label="Notifications" placeholder="Notify always">
+                <ion-select-option value="apple">Personal Notifications only</ion-select-option>
+                <ion-select-option value="banana">All Notifications</ion-select-option>
+                <ion-select-option value="orange">Notifications turned off</ion-select-option>
+            </ion-select>
+        </ion-item>
+        <ion-item>
+                <ion-select label="Inspection Reminders" placeholder="10 minutes before">
+                <ion-select-option value="apple">10 minutes before occassion</ion-select-option>
+                <ion-select-option value="banana">30 minutes before occassion</ion-select-option>
+                <ion-select-option value="orange">1 hour before occasion</ion-select-option>
+            </ion-select>
+        </ion-item>
+    </ion-list>
 </template>
+
+<script setup lang="ts">
+import {
+    IonSelect,
+    IonSelectOption,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonLabel
+} from '@ionic/vue';
+
+import { userDetailsStore } from '@/stores/userDetails';
+
+const store = userDetailsStore();
+
+const userName = () => {
+    if (store.firstName !== "") {
+        return store.firstName
+    } else {
+        return "User"
+    }
+}
+
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,10 +1,10 @@
 <template>
     <Layout>
-            <CompletedJobList />
+            <Settings />
     </Layout>
 </template>
 
 <script setup lang="ts">
     import Layout from '@/components/Layout.vue';
-    import CompletedJobList from '@/components/completedjoblist/CompletedJobList.vue';
+    import Settings from '@/components/Settings.vue';
 </script>
